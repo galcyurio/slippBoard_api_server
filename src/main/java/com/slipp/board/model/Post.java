cc5a2692a5a98e82dca373eb58bb20956cc444ee
galcyurio <galcyurio@gmail.com>
2018-08-22 01:40:48 +0900

    Post 클래스에 equals(), hashCode() 메서드 구현

--- src/main/java/com/slipp/board/model/Post.java
+++ src/main/java/com/slipp/board/model/Post.java
@@ -10,6 +10,32 @@ public class Post {
     private DateTime createDtm;
     private DateTime updateDtm;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Post post = (Post) o;
+
+        if (id != post.id) return false;
+        if (userId != null ? !userId.equals(post.userId) : post.userId != null) return false;
+        if (title != null ? !title.equals(post.title) : post.title != null) return false;
+        if (content != null ? !content.equals(post.content) : post.content != null) return false;
+        if (createDtm != null ? !createDtm.equals(post.createDtm) : post.createDtm != null) return false;
+        return updateDtm != null ? updateDtm.equals(post.updateDtm) : post.updateDtm == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (createDtm != null ? createDtm.hashCode() : 0);
+        result = 31 * result + (updateDtm != null ? updateDtm.hashCode() : 0);
+        return result;
+    }
+
     public long getId() {
         return id;
     }
