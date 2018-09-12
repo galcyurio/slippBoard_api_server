8f8c652528eac3c11c61a1e3b26d8252819019fa
galcyurio <galcyurio@gmail.com>
2018-09-12 22:21:10 +0900

    보안관련 설정 변경

--- server/src/main/kotlin/net/slipp/config/WebSecurityConfig.kt
+++ server/src/main/kotlin/net/slipp/config/WebSecurityConfig.kt
@@ -2,7 +2,7 @@ package net.slipp.config
 
 import net.slipp.service.UserService
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -16,12 +16,6 @@ class WebSecurityConfig(
     val userService: UserService
 ) : WebSecurityConfigurerAdapter() {
 
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.userDetailsService(userService)
-            .and()
-            .jdbcAuthentication()
-    }
-
     override fun configure(http: HttpSecurity) {
         // TODO: dev 일 경우만 H2 관련 설정하기
         http.authorizeRequests()
@@ -31,8 +25,12 @@ class WebSecurityConfig(
             .and()
             .headers().frameOptions().sameOrigin()
 
-        http.authorizeRequests()
+        http.userDetailsService(userService)
+            .csrf().disable()
+            .anonymous().and()
+            .authorizeRequests()
             .antMatchers("/public").permitAll()
+            .antMatchers(HttpMethod.POST, "/signOn").anonymous()
             .anyRequest().authenticated()
             .and()
             .httpBasic()
